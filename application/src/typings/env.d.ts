83edaae97b07e711977244cea94725175497f3c7
Jacint Varga <jacint.varga@dakai.io>
2023-06-15 16:03:13 +0200

    fix: deleted unused envs

--- application/src/typings/env.d.ts
+++ application/src/typings/env.d.ts
@@ -1,7 +1,5 @@
 declare module '@env' {
   export const API_BASE: string;
-  export const API_KEY_NAME: string;
-  export const API_PRIVATE_KEY: string;
   export const RPC_URL: string;
   export const CHAIN_ID: string;
   export const EXPLORER_URL: string;
